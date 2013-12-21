553c63a801a9ad61671090b8ade9926afc1ece3b
Preston Stosur <mr.junkmail89@gmail.com>
2013-12-21 17:05:31 -0600

    added theme files

---
+++ theme/js/jquery.equalHeights.js
@@ -0,0 +1,10 @@
+$.fn.equalHeights = function(px) {
+	$(this).each(function(){
+		var currentTallest = 0;
+		$(this).children().each(function(i){
+			if ($(this).height() > currentTallest) { currentTallest = $(this).height(); }
+		});
+		$(this).children().css({'min-height': currentTallest});
+	});
+	return this;
+};
